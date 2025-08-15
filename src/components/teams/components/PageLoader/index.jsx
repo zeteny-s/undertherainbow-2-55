225923813507b8e7cd82c8909a191ed28e389d82
zeteny-s <business.zeteny@gmail.com>
2025-08-15 23:37:29 +0200

    Add files via upload

---
+++ src/components/teams/components/PageLoader/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Spin } from 'antd';
+
+import { LoadingOutlined } from '@ant-design/icons';
+
+const PageLoader = () => {
+  const antIcon = <LoadingOutlined style={{ fontSize: 64 }} spin />;
+  return (
+    <div className="centerAbsolute">
+      <Spin indicator={antIcon}></Spin>
+    </div>
+  );
+};
+export default PageLoader;
